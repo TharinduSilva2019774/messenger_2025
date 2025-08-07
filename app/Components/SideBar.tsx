08713819c4ac2998b9a159f86f7ef89c966c1e67
Tharindu Silva <tsembakutti@gmail.com>
2025-08-07 21:13:06 +0100

    Minor stuff

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -1,10 +1,29 @@
 "use client";
-
+import { auth, currentUser } from '@clerk/nextjs/server'
 import styles from "./SideBar.module.css";
+import { useUser } from '@clerk/nextjs';
+import Image from 'next/image';
 
 function SideBar() {
+
+    const {user}= useUser();
+
+    const { firstName, lastName, imageUrl } = user || {
+        firstName: "",
+        lastName: "",
+        imageUrl: "",
+      };
+    console.log(user)
     return (
         <div className={styles.SideBarContainer}>
+            <div className={styles.userProfile}>
+                {user && <Image src={imageUrl} alt='profile' height="40" width="40"/>}
+                <div>
+                    <p>{firstName}</p>
+                    <p>{lastName}</p>
+                </div>
+            </div>
+
             <h1 className={styles.h1}>SideBar</h1>
         </div>
     )
